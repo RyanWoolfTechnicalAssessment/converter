781bb06bd38b5abe20a7ba56494f57578037ec30
RyanWoolfTechnicalAssessment <124464786+RyanWoolfTechnicalAssessment@users.noreply.github.com>
2023-02-06 06:18:33 +0200

    update url

--- src/app/app-service.service.ts
+++ src/app/app-service.service.ts
@@ -11,6 +11,6 @@ export class AppServiceService {
 
   getData(){
     console.log('in getData');
-    return this.http.get('https://www.ryanwoolftechnicalassessment/api/measure-units?measurementType=temperature-type&measurementAmount=15.5&measurementSystemFrom=imperial');
+    return this.http.get('https://www.ryanwoolftechnicalassessment.co.za/api/measure-units?measurementType=temperature-type&measurementAmount=15.5&measurementSystemFrom=imperial');
   }
 }
